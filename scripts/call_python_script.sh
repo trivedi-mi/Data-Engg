26cc669617929a29d93bc6a8b37cd0fd3c3a2703
trivedi-mi <90936306+trivedi-mi@users.noreply.github.com>
2023-07-14 16:30:55 -0400

    Update call_python_script.sh

--- scripts/call_python_script.sh
+++ scripts/call_python_script.sh
@@ -4,6 +4,6 @@ echo "downloding localstack dependency"
 pip install localstack-client
 
 echo  "starting python script"
-python /tmp/scripts/create_and_write_to_queue.py
+python /tmp/scripts/create_write_queue.py
 
 echo "winding down"
